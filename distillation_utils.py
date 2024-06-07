4d3c883c13fe3a4c0d9d3b7e56f689b728505fda
Emily Zhang <emily49@stanford.edu>
2024-06-07 16:58:04 -0700

    distillation

--- distillation_utils.py
+++ distillation_utils.py
@@ -6,47 +6,60 @@ from api_utils import run_inference, run_inference_prompt
 
 # this function is used to output the right formate for each row in the dataset
 def create_text_row(output, input):
-    instruction = """Given 16 words, split them into 4 groups of 4 words, so each group is related by a certain theme or category. Before you give the answers, explain why you made the grouping choices at the start of your response.
+    instruction = f"""
+# CONTEXT #
+You are playing a linguistic game that requires chain-of-thought reasoning and has only one correct answer.
+Given 16 words, split 16 words into 4 groups of 4 words, so each group is related by a certain theme or category. 
 
-Follow the restrictions at the end of the instruction.
-    
+Provide your reasoning for the groupings along with your answers.
+
+Each of the 16 words is in EXACTLY ONE of 4 groups. Each group has EXACTLY 4 words. Think through this step by step. You should start by identifying the easiest group, which is usually four items that belong in the same category of objects.
+
 Here are some hints.
-    1. The 4 words can be items that belong in the same category.
+    1. A reason 4 words are in a group can be that they are items that belong in the same category.
     Example:
     Fish: BASS, SALMON, TROUT, FLOUNDER
     Body parts: HEAD, KNEES, SHOULDER, TOES
 
-    2. The 4 words can each form a common phrase when paired with another word.
+    2. A reason 4 words are in a group can be that they each form a common phrase when paired with another word.
     Example:
     Fire __: ANT, DRILL, ISLAND, OPAL
     ___ Change: CHUMP, CLIMATE, LOOSE, SEA
 
-    3. The 4 words can be associated with a certain verb phrase or action
+    3. A reason 4 words are in a group can be that they are associated with a certain verb phrase or action.
     Example:
     Things to crack: EGG, KNUCKLES, SMILE, WINDOW
     Removes the covering of: PARES, PEELS, SHELLS, SHUCKS
-    
-    Start by identifying the easiest group, which is usually four items that belong in the same category of objects.
-    
-Make sure that your reasoning and 4 groups of 4 words follow the following format EXACTLY. Otherwise, you will suffer penalty beyond imagination.
-Only replace [EXPLAIN THIS] with a justification for your grouping choices.
-Only replace [REPLACE_THIS] with the words. Each word should only be used once. Each line is a group of 4 words related by a certain theme or category.
-    
-Format:
-    [EXPLAIN_THIS]
 
-    So the answer is:
+#########
 
-    Group 1: [REPLACE_THIS], [REPLACE_THIS], [REPLACE_THIS], [REPLACE_THIS]
-    Group 2: [REPLACE_THIS], [REPLACE_THIS], [REPLACE_THIS], [REPLACE_THIS]
-    Group 3: [REPLACE_THIS], [REPLACE_THIS], [REPLACE_THIS], [REPLACE_THIS]
-    Group 4: [REPLACE_THIS], [REPLACE_THIS], [REPLACE_THIS], [REPLACE_THIS]
+# OBJECTIVE #
 
-Double check that your response follows the above format. Make sure the [REPLACE_THIS] tokens are replaced with your words.
+Provide your reasoning and the correct 4 groups of 4 words for these 16 words: {input}
 
-Here are the 16 words: """
+#########
 
-    text_row = f"""<s>[INST] {instruction} {input} [/INST]
+# RESPONSE FORMAT #
+        
+You answer should follow the following format exactly. Otherwise you will suffer consequences beyond imagination.
+
+Respond with chain-of-thought reasoning for your grouping choices followed by "So the answer is:" and a JSON object with a single key "answer" that has a list of 4 lists, each of which has 4 words. Make sure each of the 16 words appears in EXACTLY one group and each group has EXACTLY 4 words. 
+
+DO NOT include anything after this JSON object.
+
+#########
+
+# EXAMPLE #
+Given the 16 words: SHALLOW, MAKE UP, COO, SURFACE, IMPROV, SIDE, AD-LIB, BABBLE, COSMETIC, CRAWL, DOMINO, FREESTYLE, PLACEBO, NURSE, BUTTERFLY, EXTERNAL 
+
+The correct response would be: 
+
+[REASONING_HERE]
+
+So the answer is:
+
+{{"answer": [["AD-LIB","FREESTYLE","IMPROV","MAKE UP"],["BABBLE","COO","CRAWL","NURSE"],["COSMETIC","EXTERNAL","SHALLOW","SURFACE"],["BUTTERFLY","DOMINO","PLACEBO","SIDE"]]}}"""
+    text_row = f"""<s>[INST] {instruction} [/INST]
 {output} </s>"""
     return text_row
 
@@ -128,7 +141,9 @@ def create_mistral_finetune_distillation_data(data, distillation_data, str):
     for puzzle, distillation in zip(transformed_data, distillation_data):
         entries_string = ", ".join(random.sample(puzzle["entries"], 16))  # shuffle
         answer_string = "\n".join([", ".join(group) for group in puzzle["solutions"]])
-        output = f"{distillation}\n\nSo the answer is:\n\n{answer_string}"
+        answer_obj = {"answer": puzzle["solutions"]}
+        output = f"{distillation}\n\nSo the answer is:\n\n{json.dumps(answer_obj)}"
+        # output = f"{distillation}\n\nSo the answer is:\n\n{answer_string}"
         result.append(create_text_row(output, entries_string))
 
     with open(f"data/splits/connections_{str}.jsonl", "w") as f:
